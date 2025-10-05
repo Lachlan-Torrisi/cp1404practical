1e8c05da45c5ff88f6575f8eb575f9786489778b
Lachlan-Torrisi <lachlan.torrisi@my.jcu.edu.au>
2025-10-05 13:46:14 +1000

    Add: For loop for desired output.

---
+++ prac_03/randoms.py
@@ -0,0 +1,15 @@
+import random
+
+"""
+What did you see on line 1? 17
+What was the smallest number you could have seen, what was the largest? 5, 20
+
+What did you see on line 2? 9
+What was the smallest number you could have seen, what was the largest? 3, 9
+Could line 2 have produced a 4? No, as it went up in intervals of 2.
+
+What did you see on line 3? 5.294...
+What was the smallest number you could have seen, what was the largest? 2.5, 5.5
+"""
+
+print(random.randint(1, 100))
