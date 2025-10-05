fd0dfbd6b1055152469612c180279f5e8276187f
Lachlan-Torrisi <lachlan.torrisi@my.jcu.edu.au>
2025-10-05 14:05:18 +1000

    Add: Program to read names from the file

--- prac_03/files.py
+++ prac_03/files.py
@@ -2,3 +2,10 @@ name = input("Enter your name: ")
 out_file = open("name.txt", 'w')
 print(f"Name: {name}", file=out_file)
 out_file.close()
+
+
+in_file = open("name.txt", 'r')
+text = in_file.read()
+in_file.close()
+print(text)
+
