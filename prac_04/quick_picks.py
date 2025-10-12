256afc47eae1e7a5d33d29664839aed8813250e2
Lachlan-Torrisi <lachlan.torrisi@my.jcu.edu.au>
2025-10-12 23:31:18 +1000

    Made random number pick generator without repeats

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,17 @@
+import random
+
+NUMBERS_PER_PICK = 6
+MIN = 1
+MAX = 45
+
+num_picks = int(input("How many quick picks? "))
+
+for i in range(num_picks):
+    numbers = []
+
+    while len(numbers) < NUMBERS_PER_PICK:
+        number = random.randint(MIN, MAX)
+        if number not in numbers:
+            numbers.append(number)
+
+
