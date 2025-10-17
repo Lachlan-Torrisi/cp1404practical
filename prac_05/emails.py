45b46bf385ea49309481c90f386b2bafb57cd027
Lachlan-Torrisi <lachlan.torrisi@my.jcu.edu.au>
2025-10-17 14:43:33 +1000

    Created main function for storing emails

---
+++ prac_05/emails.py
@@ -0,0 +1,14 @@
+
+
+
+def main():
+    email_to_name = {}
+    user_email = input("Email: ")
+
+    while user_email != "":
+        user_name = "Extract user name from email here (make a function for it)"
+        
+
+
+
+main()
