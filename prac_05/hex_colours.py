ab2d064c5c12a66c1fcc829a1fcb77e0364b8ce9
Lachlan-Torrisi <lachlan.torrisi@my.jcu.edu.au>
2025-10-17 13:12:00 +1000

    Added lookup colour feature, Debugged dictonary

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,10 +1,19 @@
-HEXCODE_TO_COLOUR = {"#00ffff": "Aqua",
-                     "#f5f5dc": "Beige",
-                     "#000000": "Black",
-                     "#79443b": "Bole",
-                     "#b5a642": "Brass",
-                     "#a52a2a": "Brown",
-                     "#ffff99": "Canary",
-                     "#b2ffff": "Celeste",
-                     "#36454f": "Charcoal",
-                     "#954535": "Chestnut"}
+HEXCODE_TO_COLOUR = {"AQUA": "#00ffff",
+                     "BEIGE": "#f5f5dc",
+                     "BLACK": "#000000",
+                     "BOLE": "#79443b",
+                     "BRASS": "#b5a642",
+                     "BROWN": "#a52a2a",
+                     "CANARY": "#ffff99",
+                     "CELESTE": "#b2ffff",
+                     "CHARCOAL": "#36454f",
+                     "CHESTNUT": "#954535"}
+
+colour_name = input("Enter colour: ").upper()
+while colour_name != "":
+    try:
+        colour = HEXCODE_TO_COLOUR[colour_name]
+        print(colour)
+    except KeyError:
+        print("Invalid colour")
+    colour_name = input("Enter colour: ").upper()
