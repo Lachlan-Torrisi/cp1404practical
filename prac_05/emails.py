8d95e59df054c9294ec83792a01c32d935bb1988
Lachlan-Torrisi <lachlan.torrisi@my.jcu.edu.au>
2025-10-19 20:38:42 +1000

    Made name verifier

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,6 +7,9 @@ def main():
 
     while user_email != "":
         user_name = get_name_from_email(user_email)
+        verification = input(f"Is your name {user_name}? (Y/n) ").upper()
+        if verification != "" and verification != "Y":
+            user_name = input("Name: ")
         print(user_name)
 
 def get_name_from_email(user_email):
