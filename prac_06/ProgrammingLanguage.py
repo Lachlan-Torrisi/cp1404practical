5afdeef91d823f36c89bf1dc9fe0221c0a4f3716
Lachlan-Torrisi <lachlan.torrisi@my.jcu.edu.au>
2025-10-29 15:38:32 +1000

    tested code

---
+++ prac_06/ProgrammingLanguage.py
@@ -0,0 +1,16 @@
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Return True if the language uses dynamic typing."""
+        return self.typing.lower() == "dynamic"
+
+    def __str__(self):
+        """Return a nicely formatted string for this language."""
+        return (f"{self.name}, {self.typing} Typing, "
+                f"Reflection={self.reflection}, First appeared in {self.year}")
