4f3c10b0b6aaaedf2e8f6575cfd43791b86ceb5f
Lachlan-Torrisi <lachlan.torrisi@my.jcu.edu.au>
2025-10-31 10:53:17 +1000

    Made the initial class setup

---
+++ prac_06/guitar.py
@@ -0,0 +1,15 @@
+"""
+Estimate: 15 mins
+Actual:
+
+"""
+
+
+class Guitar:
+    """Represent a guitar with name, year, and cost."""
+
+    def __init__(self, name="", year=0, cost=0.0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
