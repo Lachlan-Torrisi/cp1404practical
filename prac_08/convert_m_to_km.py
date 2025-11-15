2d2a5deae9cae44535b2ec07616054f5cafcec15
Lachlan-Torrisi <lachlan.torrisi@my.jcu.edu.au>
2025-11-15 12:13:04 +1000

    Added functionality to the buttons

--- prac_08/convert_m_to_km.py
+++ prac_08/convert_m_to_km.py
@@ -6,12 +6,27 @@ MILES_TO_KM = 1.60934
 
 
 class MilesConverterApp(App):
-    """ MilesConverterApp is a Kivy App for converting miles to kilometres """
     def build(self):
-        """ build the Kivy app from the kv file """
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_m_km.kv')
         return self.root
 
+    def handle_convert(self):
+        miles = self.get_valid_miles()
+        km = miles * MILES_TO_KM
+        self.root.ids.output_label.text = f"{km:.5f}"
+
+    def handle_increment(self, amount):
+        miles = self.get_valid_miles()
+        miles += amount
+        self.root.ids.input_miles.text = str(miles)
+        self.handle_convert()
+
+    def get_valid_miles(self):
+        text = self.root.ids.input_miles.text
+        try:
+            return float(text)
+        except ValueError:
+            return 0.0
 
 MilesConverterApp().run()
